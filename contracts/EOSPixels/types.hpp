c7c0ba21c5424a833793c546a1ddc1ae6633b11a
bookcool <sunshuhan666@163.com>
2018-10-18 11:28:42 +0800

    update pot percentage

--- contracts/EOSPixels/types.hpp
+++ contracts/EOSPixels/types.hpp
@@ -49,7 +49,7 @@ struct pixel {
     //
     // Just to maintain sanity, we artificially limit the ceiling of a pixel
     // price to below 90 EOS.
-    eosio_assert(priceCounter <= 25, "price for pixel is too high");
+    eosio_assert(priceCounter <= 50, "price for pixel is too high");
 
     return currentPrice() * PRICE_MULTIPLIER;
   }
